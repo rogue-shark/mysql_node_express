81b4f13a640bd9c8a775f4ad6ba045c2917b4dcc
RogueShark <shantanuss789@gmail.com>
2023-06-26 23:40:19 +0530

    more routes

--- app.js
+++ app.js
@@ -3,9 +3,23 @@ import { getAllNotes, getNote, createNote } from './database.js';
 
 const app = express()
 
+app.use(express.json()) // POSTing as JSON on post route
+
 app.get('/notes', async (req, res) => {
     const notes = await getAllNotes()
     res.send(notes)
+})
+
+app.get('/notes/:id', async (req, res) => {
+    const id = req.params.id
+    const note = await getNote(id)
+    res.send(note)
+})
+
+app.post('/notes', async (req, res) => {
+    const {title, contents} = req.body
+    const note = await createNote(title, contents)
+    res.status(201).send(note)
 })
 
 //handle error
