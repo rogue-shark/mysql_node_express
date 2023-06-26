24bc038dff25e00025ffa31810c4b23dc2461d9e
RogueShark <shantanuss789@gmail.com>
2023-06-26 23:04:34 +0530

    More query functions

--- database.js
+++ database.js
@@ -9,10 +9,37 @@ const pool = mysql.createPool({
     database: process.env.MYSQL_DATABASE
   }).promise()
 
-async function getAllnotes() {
+export async function getAllNotes() {
     const [rows] = await pool.query("select * from notes")
     return rows
 }
-  
 
+/* ❌ Unsecure -- SQL Injection [https://www.youtube.com/watch?v=cx6Xs3F_1Uc]
+To prevent this, we should use prepared statements. This means putting a quesion mark where the potentially unsafe value should be and passing the value to the query function in an array.
+
+export async function getNote(id) {
+    const [rows] = await pool.query(`
+    SELECT * 
+    FROM notes
+    WHERE id = ${id}
+    `)
+    return rows[0]
+} */
+  
+export async function getNote(id) {
+    const [rows] = await pool.query(`
+    SELECT * 
+    FROM notes
+    WHERE id = ${id}
+    `)
+    return rows
+}
+
+export async function createNote(title, contents) {
+    const [result] = await pool.query(`
+    INSERT INTO notes (title, contents)
+    VALUES (?, ?)
+    `, [title, contents])
+    return result
+}
 
